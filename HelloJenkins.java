b84fbb55d5f0f4d8c242ed4bd3ce29ee8d6f1ad1
sh3ngsh3ng <64950350+sh3ngsh3ng@users.noreply.github.com>
2022-09-16 16:43:55 +0800

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,5 +1,5 @@
 public class HelloJenkins {
    public static void main(String[] args) {
-      System.out.println("Hello Jenkins!, I am Github!") 
+      System.out.println("Hello Jenkins!, I am Github!");
    }
 }
